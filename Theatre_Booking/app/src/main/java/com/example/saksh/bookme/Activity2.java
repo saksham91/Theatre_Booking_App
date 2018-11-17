14396a101320d8e3511b9c45839254e954b9d3d5
saksham91 <38701227+saksham91@users.noreply.github.com>
2018-11-17 17:56:15 -0500

    Modified 2nd Activity
    Added a datepicker and set its min and max values

--- Theatre_Booking/app/src/main/java/com/example/saksh/bookme/Activity2.java
+++ Theatre_Booking/app/src/main/java/com/example/saksh/bookme/Activity2.java
@@ -1,15 +1,54 @@
 package com.example.saksh.bookme;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class Activity2 extends AppCompatActivity {
+
+    DatePickerDialog picker;
+    EditText eText;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        eText=findViewById(R.id.edit1);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(Activity2.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.getDatePicker().setMinDate(cldr.getTimeInMillis());
+                picker.getDatePicker().setMaxDate(cldr.getTimeInMillis()+ 518400000);
+                picker.show();
+
+            }
+        });
     }
+
 
     public void go_back(View view){
         Intent intent = new Intent(Activity2.this, MainActivity.class);
